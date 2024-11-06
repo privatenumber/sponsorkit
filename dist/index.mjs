819b389d90723538a616944323372280fbfde539
Hiroki Osame <hiroki.osame@gmail.com>
2024-11-06 14:51:33 +0900

    Published branch "svg-webp"

--- dist/index.mjs
+++ dist/index.mjs
@@ -1,41 +1,10 @@
-import jiti from "file:///Users/osame/Developer/github/antfu-collective/sponsorkit/node_modules/.pnpm/jiti@1.21.6/node_modules/jiti/lib/index.js";
-
-/** @type {import("/Users/osame/Developer/github/antfu-collective/sponsorkit/src/index")} */
-const _module = jiti(null, {
-  "esmResolve": true,
-  "interopDefault": true,
-  "alias": {
-    "sponsorkit": "/Users/osame/Developer/github/antfu-collective/sponsorkit"
-  }
-})("/Users/osame/Developer/github/antfu-collective/sponsorkit/src/index.ts");
-
-export const defineConfig = _module.defineConfig;
-export const loadConfig = _module.loadConfig;
-export const partitionTiers = _module.partitionTiers;
-export const tiers = _module.tiers;
-export const includePastSponsors = _module.includePastSponsors;
-export const defaultTiers = _module.defaultTiers;
-export const defaultInlineCSS = _module.defaultInlineCSS;
-export const defaultConfig = _module.defaultConfig;
-export const FALLBACK_AVATAR = _module.FALLBACK_AVATAR;
-export const tierPresets = _module.tierPresets;
-export const presets = _module.presets;
-export const resolveAvatars = _module.resolveAvatars;
-export const round = _module.round;
-export const radius = _module.radius;
-export const size = _module.size;
-export const svgToPng = _module.svgToPng;
-export const svgToWebp = _module.svgToWebp;
-export const genSvgImage = _module.genSvgImage;
-export const y = _module.y;
-export const base64Image = _module.base64Image;
-export const generateBadge = _module.generateBadge;
-export const SvgComposer = _module.SvgComposer;
-export const ProvidersMap = _module.ProvidersMap;
-export const guessProviders = _module.guessProviders;
-export const resolveProviders = _module.resolveProviders;
-export const fetchSponsors = _module.fetchSponsors;
-export const GitHubProvider = _module.GitHubProvider;
-export const fetchGitHubSponsors = _module.fetchGitHubSponsors;
-export const makeQuery = _module.makeQuery;
-export const outputFormats = _module.outputFormats;
+export { F as FALLBACK_AVATAR, G as GitHubProvider, P as ProvidersMap, S as SvgComposer, h as defaultConfig, f as defaultInlineCSS, e as defaultTiers, d as defineConfig, n as fetchGitHubSponsors, m as fetchSponsors, k as genSvgImage, g as generateBadge, a as guessProviders, l as loadConfig, q as makeQuery, o as outputFormats, p as partitionTiers, i as presets, j as resizeImage, b as resolveAvatars, r as resolveProviders, s as svgToPng, c as svgToWebp, t as tierPresets } from './shared/sponsorkit.a8cb94ba.mjs';
+import 'unconfig';
+import 'node:process';
+import 'dotenv';
+import 'node:buffer';
+import 'consola';
+import 'ofetch';
+import 'sharp';
+import 'node:crypto';
+import 'normalize-url';
